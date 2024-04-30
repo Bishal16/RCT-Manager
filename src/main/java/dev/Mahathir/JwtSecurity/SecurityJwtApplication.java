48bfa770efdd9a4de753d842a8f32af761baf364
ashik <ashikmahmud691998@gmail.com>
2024-04-30 13:37:45 +0600

    seperate db properties file working

--- src/main/java/dev/Mahathir/JwtSecurity/SecurityJwtApplication.java
+++ src/main/java/dev/Mahathir/JwtSecurity/SecurityJwtApplication.java
@@ -3,9 +3,12 @@ package dev.Mahathir.JwtSecurity;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.PropertySource;
+
 //
 @SpringBootApplication
 @EnableDiscoveryClient
+@PropertySource("classpath:application-db.properties")
 public class SecurityJwtApplication {
 
 	public static void main(String[] args) {
